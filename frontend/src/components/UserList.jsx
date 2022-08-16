a3af37556115a3cba49798d585322016a5b4f79c
hawik <hawiksyaef@gmail.com>
2022-08-16 23:05:50 +0700

    konfigurasi get user pada frontend

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+
 const UserList = () => {
 
-  return (
-    <div className="columns mt-5 is-centered">
-        <div className='column is-half'>
-            <table className='table is-striped is-fullwidth'>
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Nama</th>
-                        <th>Email</th>
-                        <th>Gender</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+      getUser();
+
+    }, [])
+
+    const getUser = async () => {
+        const response = await axios.get('http://localhost:5000/users');
+        setUser(response.data);
+    }
+
+    return (
+        <div className="columns mt-5 is-centered">
+            <div className='column is-half'>
+                <table className='table is-striped is-fullwidth'>
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th>Gender</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {user.map((user, index) => (
+                        <tr key={user.id}>
+                            <td>{index + 1}</td>
+                            <td>{user.nama}</td>
+                            <td>{user.email}</td>
+                            <td>{user.gender}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
   )
 }
 
